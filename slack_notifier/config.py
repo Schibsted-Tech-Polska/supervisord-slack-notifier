b33c77fe85f9a7abb5316dd834a90bcb6e57d84d
Wojciech Iskra <wojciech.iskra@schibsted.pl>
2015-09-04 16:10:47 +0200

    Removed duplicated structure, switche emoji to default ones

--- slack_notifier/config.py
+++ slack_notifier/config.py
@@ -1,34 +1,27 @@
 programs = []
-events = [
-    'PROCESS_STATE_BACKOFF',
-    'PROCESS_STATE_FATAL',
-    'PROCESS_STATE_EXITED',
-    'PROCESS_STATE_STOPPED',
-    'PROCESS_STATE_STARTING',
-    'PROCESS_STATE_RUNNING']
-eventMap = dict(
+events = dict(
     PROCESS_STATE_BACKOFF=dict(
         color='danger',
-        emoji=':sadpanda:',
+        emoji=':fire:',
     ),
     PROCESS_STATE_FATAL=dict(
         color='danger',
-        emoji=':sadpanda:',
+        emoji=':fire:',
     ),
     PROCESS_STATE_EXITED=dict(
         color='danger',
-        emoji=':sadpanda:',
+        emoji=':fire:',
     ),
     PROCESS_STATE_STOPPED=dict(
         color='warning',
-        emoji=':bambo:',
+        emoji=':stop:',
     ),
     PROCESS_STATE_STARTING=dict(
         color='good',
-        emoji=':fuckyeah:',
+        emoji=':+1:',
     ),
     PROCESS_STATE_RUNNING=dict(
         color='good',
-        emoji=':fuckyeah:',
+        emoji=':+1:',
     ),
 )
